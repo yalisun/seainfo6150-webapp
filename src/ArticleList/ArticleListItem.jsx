b3a3db3d7342d2e37d77932a987a0ae4d457d13f
Yali Sun <zixiao.yali@gmail.com>
2020-02-22 19:00:45 -0800

    add week-7

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -6,21 +6,19 @@ import ArticleImage from "./ArticleImage";
 
 const ArticleListItem = props => {
   return (
-    <html>
+    <section className={styles.flexContainer}>
+      <div className={styles.image}>
+        <ArticleImage url={props.article.image._url} title={props.article.title} />
+      </div>
       <div>
-        <body>
-          <ArticleImage url={props.article.image._url} title={props.article.title} />
-          <section>
-            <h1 className={styles.title}>{props.article.title}</h1>
+        <h1 className={styles.title}>{props.article.title}</h1>
             <p>{props.article.shortText}</p>
             <time dateTime={props.article.pubYear}>{props.article.pubDate}</time>
-            <div id="button">
-              {<SlugButton slug={props.article.slug} buttonText={props.article.author} />}
-            </div>
-          </section>
-        </body>
       </div>
-    </html>
+      <div id="button">
+        {<SlugButton slug={props.article.slug} buttonText={props.article.author} />}
+      </div>
+    </section>
   );
 };
 
